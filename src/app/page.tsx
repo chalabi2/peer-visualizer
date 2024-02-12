1ef02177be4d3851f9d8d6b4353aaf7759ea648e
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2024-02-12 11:52:04 -0700

    add pie chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { Tooltip } from "react-leaflet";
+import { ResponsiveContainer, PieChart, Pie } from "recharts";
 
 interface IPeer {
   ip: string;
@@ -21,11 +23,12 @@ interface IGroupedPeers {
 }
 
 export default function Home() {
-  const [inputIp, setInputIp] = useState("");
   const [groupedPeers, setGroupedPeers] = useState<IGroupedPeers>({});
+  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
+  const [pieData, setPieData] = useState<any[]>([]);
 
   const fetchPeers = async () => {
-    const response = await fetch(`/api/queryNode?nodeIP=${inputIp}`);
+    const response = await fetch(`/api/queryNode?`);
     const data: IPeer[] = await response.json();
     if (Array.isArray(data)) {
       const grouped = data.reduce<IGroupedPeers>(
@@ -49,48 +52,87 @@ export default function Home() {
     fetchPeers();
   }, []);
 
+  useEffect(() => {
+    // Aggregate total IPs for each country
+    const newData = Object.entries(groupedPeers).map(
+      ([country, { totalIPs }]) => ({
+        name: country,
+        value: totalIPs,
+      })
+    );
+    setPieData(newData);
+  }, [groupedPeers]);
+
+  // Example of selecting a country, could be set from a dropdown or another UI element
+  useEffect(() => {
+    const firstCountry = Object.keys(groupedPeers)[0];
+    setSelectedCountry(firstCountry);
+  }, [groupedPeers]);
+
   return (
-    <main className="flex min-h-screen flex-col mx-auto items-center justify-center p-4">
+    <main className="flex min-h-screen flex-col overflow-clip mx-auto items-center justify-center p-4">
+      <div className="mt-8 w-full max-w-4xl ">
+        <ResponsiveContainer width="100%" height={400}>
+          <PieChart>
+            <Pie
+              data={pieData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              outerRadius={150}
+              fill="#8884d8"
+              dataKey="value"
+              nameKey="name"
+              label={({ name, percent }) =>
+                `${name}: ${(percent * 100).toFixed(0)}%`
+              }
+            ></Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
       <div className="mt-8 w-full max-w-4xl ">
         <h1 className="text-2xl font-bold mb-4">
           Peers Grouped by Country and ISP
         </h1>
-        {Object.entries(groupedPeers).map(
-          ([country, { totalIPs, isps }], countryIndex) => (
-            <React.Fragment key={countryIndex}>
-              <h2 className="text-xl font-semibold mt-6 mb-2">{`${country} (${totalIPs})`}</h2>
-              <table className="text-black table-auto w-full">
-                <thead className="bg-gray-200">
-                  <tr>
-                    <th className="px-4 py-2">ISP</th>
-                    <th className="px-4 py-2">IP Address</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {Object.entries(isps).flatMap(([isp, ips], ispIndex) => [
-                    ...ips.map((ip, ipIndex) => (
-                      <tr
-                        key={`${ispIndex}-${ipIndex}`}
-                        className={`${
-                          ipIndex % 2 === 0 ? "bg-gray-100" : "bg-white"
-                        }`}
-                      >
-                        <td className="border px-4 py-2">
-                          {ipIndex === 0 && ips.length > 1
-                            ? `${isp} (${ips.length})`
-                            : ipIndex === 0
-                            ? isp
-                            : ""}
-                        </td>
-                        <td className="border px-4 py-2">{ip}</td>
-                      </tr>
-                    )),
-                  ])}
-                </tbody>
-              </table>
-            </React.Fragment>
-          )
-        )}
+        <div className="items-center max-h-screen overflow-y-auto justify-between">
+          {Object.entries(groupedPeers).map(
+            ([country, { totalIPs, isps }], countryIndex) => (
+              <React.Fragment key={countryIndex}>
+                <h2 className="text-xl font-semibold mt-6 mb-2">{`${country} (${totalIPs})`}</h2>
+                <table className="text-black table-auto w-full">
+                  <thead className="bg-gray-200">
+                    <tr>
+                      <th className="px-4 py-2">ISP</th>
+                      <th className="px-4 py-2">IP Address</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {Object.entries(isps).flatMap(([isp, ips], ispIndex) => [
+                      ...ips.map((ip, ipIndex) => (
+                        <tr
+                          key={`${ispIndex}-${ipIndex}`}
+                          className={`${
+                            ipIndex % 2 === 0 ? "bg-gray-100" : "bg-white"
+                          }`}
+                        >
+                          <td className="border px-4 py-2">
+                            {ipIndex === 0 && ips.length > 1
+                              ? `${isp} (${ips.length})`
+                              : ipIndex === 0
+                              ? isp
+                              : ""}
+                          </td>
+                          <td className="border px-4 py-2">{ip}</td>
+                        </tr>
+                      )),
+                    ])}
+                  </tbody>
+                </table>
+              </React.Fragment>
+            )
+          )}
+        </div>
       </div>
     </main>
   );
